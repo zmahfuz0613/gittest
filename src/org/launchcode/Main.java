7e7f1932222c89ee8abfc28bf43829c99544337a
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-10-05 19:22:32 -0400

    This is a github homework

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -5,9 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+        System.out.println("Chandrika");
+        System.out.println("This is a github homework");
 
+
+        int x = 10;
+
+        while (x < 20) {
+            System.out.print("value of x : " + x);
+            x++;
+            System.out.print("\n");
+
+        }
     }
 }
 
