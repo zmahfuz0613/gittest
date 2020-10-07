9f1152036e536d85bd47d45887d8ea5f8a6cf156
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-10-06 21:51:29 -0400

    Arrays

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -72,8 +72,37 @@ public class Main {
                 System.out.println("Invalid grade");
         }
         System.out.println("Your grade is " + highestNumber);
-    }
 
+        //Strings
+
+        String palindrome = "Dot saw I was Tod";
+        int len = palindrome.length();
+        System.out.println( "String Length is : " + len );
+
+
+        //Arrays and numbers
+
+        double[] myList = {1.9, 2.9, 3.4, 3.5};
+
+        // Print all the array elements
+        for (int i = 0; i < myList.length; i++) {
+            System.out.println(myList[i] + " ");
+        }
+
+        // Summing all elements
+        double total = 0;
+        for (int i = 0; i < myList.length; i++) {
+            total += myList[i];
+        }
+        System.out.println("Total is " + total);
+
+        // Finding the largest element
+        double max = myList[0];
+        for (int i = 1; i < myList.length; i++) {
+            if (myList[i] > max) max = myList[i];
+        }
+        System.out.println("Max is " + max);
+    }
 
     }
 
